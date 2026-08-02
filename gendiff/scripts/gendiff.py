8ed3bbe7450b1dcbbce1bca0dab9a1a2ad826d15
Ruslan Atpaev <rus16.steam@gmail.com>
2026-08-02 18:42:41 +0000

    Add output format option

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,6 +9,13 @@ def parse_args():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f',
+        '--format',
+        metavar='FORMAT',
+        default='stylish',
+        help='set format of output',
+    )
     return parser.parse_args()
 
 def main():
