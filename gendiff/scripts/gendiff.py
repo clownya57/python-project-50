7d9a2927571d7b49ae2ac997a87748cc0d378d75
Ruslan Atpaev <rus16.steam@gmail.com>
2026-08-02 17:02:04 +0000

    Add gendiff CLI help

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+import argparse
+
+DESCRIPTION = 'Compares two configuration files and shows a difference.'
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description=DESCRIPTION,
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    return parser.parse_args()
+
+def main():
+    parse_args()
+
+if __name__ == '__main__':
+    main()
