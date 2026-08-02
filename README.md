7f0b19a446d9d0fb9f5fce5e4344f9c61651d849
Ruslan Atpaev <rus16.steam@gmail.com>
2026-08-02 23:17:14 +0000

    Finalize project tests and cleanup

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 [![Python CI](https://github.com/clownya57/python-project-50/actions/workflows/pyci.yml/badge.svg)](https://github.com/clownya57/python-project-50/actions/workflows/pyci.yml)
 [![Quality gate status](https://sonarcloud.io/api/project_badges/measure?project=clownya57_python-project-50&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=clownya57_python-project-50)
 [![Coverage](https://sonarcloud.io/api/project_badges/measure?project=clownya57_python-project-50&metric=coverage)](https://sonarcloud.io/summary/new_code?id=clownya57_python-project-50)
-### Json Demonstration
+### JSON Demonstration
 [![asciicast](https://asciinema.org/a/WPyhFSqxt1bnokP1.svg)](https://asciinema.org/a/WPyhFSqxt1bnokP1)
-### Yaml Demostration
+### YAML Demostration
 [![asciicast](https://asciinema.org/a/6hZ6dCtwR5860C4x.svg)](https://asciinema.org/a/6hZ6dCtwR5860C4x)
 ### Recursive comparison demonstration
 [![asciicast](https://asciinema.org/a/eCvPOjjCqgLECNm2.svg)](https://asciinema.org/a/eCvPOjjCqgLECNm2)
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -9,30 +9,7 @@ from gendiff.scripts.gendiff import main
 
 TEST_DATA_DIR = Path(__file__).parent / 'test_data'
 
-STYLISH_CASES = [
-    (
-        'file1.json',
-        'file2.json',
-        'expected_stylish.txt',
-    ),
-    (
-        'file1.yml',
-        'file2.yaml',
-        'expected_stylish.txt',
-    ),
-    (
-        'nested_file1.json',
-        'nested_file2.json',
-        'expected_nested_stylish.txt',
-    ),
-    (
-        'nested_file1.yml',
-        'nested_file2.yaml',
-        'expected_nested_stylish.txt',
-    ),
-]
-
-PLAIN_CASES = [
+INPUT_CASES = [
     (
         'nested_file1.json',
         'nested_file2.json',
@@ -43,18 +20,18 @@ PLAIN_CASES = [
     ),
 ]
 
-CLI_CASES = [
+FORMAT_CASES = [
     (
         'stylish',
-        'nested_file1.json',
-        'nested_file2.json',
         'expected_nested_stylish.txt',
     ),
     (
         'plain',
-        'nested_file1.json',
-        'nested_file2.json',
         'expected_plain.txt',
+    ),
+    (
+        'json',
+        'expected_json.json',
     ),
 ]
 
@@ -65,60 +42,77 @@ def get_test_data_path(filename):
 
 def read_test_data(filename):
     filepath = get_test_data_path(filename)
-    return filepath.read_text(encoding='utf-8').rstrip('\n')
+    return filepath.read_text(
+        encoding='utf-8',
+    ).rstrip('\n')
+
+
+def assert_result(actual, expected, format_name):
+    if format_name == 'json':
+        assert json.loads(actual) == json.loads(expected)
+    else:
+        assert actual == expected
 
 
 @pytest.mark.parametrize(
-    ('file1_name', 'file2_name', 'expected_name'),
-    STYLISH_CASES,
+    ('file1_name', 'file2_name'),
+    INPUT_CASES,
 )
-def test_generate_diff_stylish(
+@pytest.mark.parametrize(
+    ('format_name', 'expected_name'),
+    FORMAT_CASES,
+)
+def test_generate_diff(
     file1_name,
     file2_name,
+    format_name,
     expected_name,
 ):
     file1 = get_test_data_path(file1_name)
     file2 = get_test_data_path(file2_name)
     expected = read_test_data(expected_name)
 
-    assert generate_diff(file1, file2) == expected
-    assert generate_diff(file1, file2, 'stylish') == expected
+    actual = generate_diff(
+        file1,
+        file2,
+        format_name,
+    )
+
+    assert_result(actual, expected, format_name)
 
 
 @pytest.mark.parametrize(
     ('file1_name', 'file2_name'),
-    PLAIN_CASES,
+    INPUT_CASES,
 )
-def test_generate_diff_plain(
-    file1_name,
-    file2_name,
-):
+def test_default_format(file1_name, file2_name):
     file1 = get_test_data_path(file1_name)
     file2 = get_test_data_path(file2_name)
-    expected = read_test_data('expected_plain.txt')
+    expected = read_test_data(
+        'expected_nested_stylish.txt',
+    )
 
-    assert generate_diff(file1, file2, 'plain') == expected
+    actual = generate_diff(file1, file2)
+
+    assert actual == expected
 
 
 @pytest.mark.parametrize(
-    (
-        'format_name',
-        'file1_name',
-        'file2_name',
-        'expected_name',
-    ),
-    CLI_CASES,
+    ('format_name', 'expected_name'),
+    FORMAT_CASES,
 )
 def test_cli(
     monkeypatch,
     capsys,
     format_name,
-    file1_name,
-    file2_name,
     expected_name,
 ):
-    file1 = get_test_data_path(file1_name)
-    file2 = get_test_data_path(file2_name)
+    file1 = get_test_data_path(
+        'nested_file1.json',
+    )
+    file2 = get_test_data_path(
+        'nested_file2.json',
+    )
     expected = read_test_data(expected_name)
 
     monkeypatch.setattr(
@@ -135,85 +129,6 @@ def test_cli(
 
     main()
 
-    captured = capsys.readouterr()
+    actual = capsys.readouterr().out.rstrip('\n')
 
-    assert captured.out == f'{expected}\n'
-
-
-@pytest.mark.parametrize(
-    ('file1_name', 'file2_name'),
-    [
-        (
-            'nested_file1.json',
-            'nested_file2.json',
-        ),
-        (
-            'nested_file1.yml',
-            'nested_file2.yaml',
-        ),
-    ],
-)
-def test_generate_diff_json(
-    file1_name,
-    file2_name,
-):
-    file1 = get_test_data_path(file1_name)
-    file2 = get_test_data_path(file2_name)
-
-    result = generate_diff(
-        file1,
-        file2,
-        'json',
-    )
-
-    parsed_result = json.loads(result)
-
-    assert isinstance(parsed_result, list)
-    assert [
-        node['key']
-        for node in parsed_result
-    ] == [
-        'common',
-        'group1',
-        'group2',
-        'group3',
-    ]
-
-
-def test_json_formats_have_same_data():
-    json_result = generate_diff(
-        get_test_data_path('nested_file1.json'),
-        get_test_data_path('nested_file2.json'),
-        'json',
-    )
-    yaml_result = generate_diff(
-        get_test_data_path('nested_file1.yml'),
-        get_test_data_path('nested_file2.yaml'),
-        'json',
-    )
-
-    assert json.loads(json_result) == json.loads(yaml_result)
-
-
-def test_cli_json(monkeypatch, capsys):
-    file1 = get_test_data_path('nested_file1.json')
-    file2 = get_test_data_path('nested_file2.json')
-
-    monkeypatch.setattr(
-        sys,
-        'argv',
-        [
-            'gendiff',
-            '--format',
-            'json',
-            str(file1),
-            str(file2),
-        ],
-    )
-
-    main()
-
-    captured = capsys.readouterr()
-    parsed_result = json.loads(captured.out)
-
-    assert isinstance(parsed_result, list)
+    assert_result(actual, expected, format_name)
